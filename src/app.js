ad65c4f72a82489ea59f82b2860b653e7662332b
BachhavPritesh <pritesh.v.bachhav.cg@gmail.com>
2026-04-18 19:08:25 +0530

    Add CRUD assignment code

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 const noteRoutes = require('./routes/note.routes');
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api', noteRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
